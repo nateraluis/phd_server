1fbe3e1ad63b0b016463236c9db311593f137a4d
Luis Guillermo Natera Orozco <nateraluis@gmail.com>
2019-02-13 15:04:44 +0100

    multiplex with bus

--- CreateMultiplex.py
+++ CreateMultiplex.py
@@ -25,7 +25,7 @@ def load_data(name):
     area = gpd.read_file('../Data/{}/{}_shape'.format(name, name))
     area = ox.project_gdf(area, to_crs={'datum': 'WGS84', 'ellps': 'WGS84', 'proj': 'utm', 'zone': 34, 'units': 'm'})
 
-    path = '{}/{}_gtfs.zip'.format(name, name.lower)
+    path = '{}/budapest_gtfs.zip'.format(name)
 
     # Automatically identify the busiest day and read that in as a Partidge feed
     feed = pt.get_representative_feed(path)
